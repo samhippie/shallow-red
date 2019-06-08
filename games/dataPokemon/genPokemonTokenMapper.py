656a299c825290ff6f4275080e735f6d1c1aec24
Sam Higginbotham <samhggnbthm@gmail.com>
2019-06-08 10:08:33 -0500

    Gen mapper now normalizes names more

--- games/dataPokemon/genPokemonTokenMapper.py
+++ games/dataPokemon/genPokemonTokenMapper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+import re
 
 #load showdown data
 with open('pokedex.json', 'r') as f:
@@ -9,6 +10,10 @@ with open('moves.json', 'r') as f:
     moves = json.load(f)
 
 tokenMapper = {}
+
+#for both moves and pokemon, some things use the id instead of species/name
+#e.g. icebeam instead of ice beam
+#so I just map both to the same thing
 
 #replace each instance of a pokemon with a basic summary
 for id in dex:
@@ -27,16 +32,19 @@ for id in dex:
         str(base['spd']),
         str(base['spe']),
     ]
-    tokenMapper[species] = [species] + types# + stats
+    tokenMapper[species] = [id] + types# + stats
+    tokenMapper[id] = [id] + types# + stats
 
 #replace each instance of a move with basic summary
 for id in moves:
     move = moves[id]
     name = move['name'].lower()
+    stripName = re.sub(r'[^a-z0-9]', '', name)
     type = move['type'].lower()
     cat = move['category'].lower()
     power = str(move['basePower'])
-    tokenMapper[name] = [name, type, cat, power]
+    tokenMapper[name] = [id, type, cat, power]
+    tokenMapper[id] = [id, type, cat, power]
 
 
 #split up level so we can see the number
